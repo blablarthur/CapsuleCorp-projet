087a2c6915818b787dd56b6e11b3d9a15996e260
Florent-gibart <32912736+Florent-gibart@users.noreply.github.com>
2018-03-15 23:58:25 +0100

    Add files via upload

---
+++ Assets/ChickenController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ChickenController : NetworkBehaviour
+{
+    public Transform t;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float minAnimationTime = 2.0f;
+    [SerializeField] float maxAnimationTime = 5.0f;
+    float animationTime = 0;
+    float time = 0;
+    Vector3 moveDirection;
+
+    Animator animator;
+    bool isIdle = true;
+
+	
+	
+	void FixedUpdate () {
+        time += Time.deltaTime;
+        if (!isLocalPlayer)
+        {
+            return;
+            
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        Debug.Log("x: " + x + "  z: "+ z);
+        t.Rotate(0, x, 0);
+        t.Translate(0, 0, z);
+
+
+    }
+
+    
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    
+    public Transform t;
+    [SerializeField] float speed = 5.0f;
+    float animationTime = 0;
+    float time = 0;
+    Vector3 moveDirection;
+    Camera c;
+    Animator animator;
+    bool isIdle = true;
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+
+       
+
+
+
+
+        time += Time.deltaTime;
+       
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed * 3.0f;
+
+        t.Rotate(0, x, 0);
+        t.Translate(0, 0, z);
+
+
+    }
+
+
+
+}
